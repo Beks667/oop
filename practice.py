f3bef194360a2a7d8916f43f419c5f9f7ac27abe
Ваше Имя <you@example.com>
2021-05-04 19:41:18 +0600

    beks

---
+++ practice.py
@@ -0,0 +1,14 @@
+class Phone:
+    name = "Iphone"
+    model = "XS"
+    year = 2018
+
+    def func(self):
+        print ('Телефон в налтчии!')
+
+    def func2 (self):
+        print ('телефон не в наличии')
+
+phone_1 =Phone()
+
+print(phone_1.func())
